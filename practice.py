f53e43c37feec475172464db3039b57ebe9f8f3f
Tommy Adgate <rosco.adgate@gmail.com>
2025-01-16 08:49:28 -0800

    Setting up workspace.

---
+++ practice.py
@@ -0,0 +1,15 @@
+'''
+Okay, so according to ChatGPT, if I wanted to master Python, there would be 7 steps. Which are listed below.
+Step 1: Master Python Basics
+Step 2: Build Small Projects
+Step 3: Explore Python Libraries
+Step 4: Learn A Framework
+Step 5: Work On Bigger Projects
+Step 6: Learn Best Practices
+Step 7: Expirement With New Technologies
+
+So let's begin.
+'''
+
+#1/16/2025  8:36AM PST
+#According to line 3. Step 1 in the process is Mastering Python Basics, which makes sense to me. So what is the first basic that i want to focus on? Or maybe the question is what are the basics? According to chatgpt Syntax and Basic Constructs are #1 so that's where I'll start. Just not right now because I want to smoke a ciggarette and watch TV.

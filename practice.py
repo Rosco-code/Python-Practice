0c96317355285290c6f23d6f3c8dcbda6d5a3898
Tommy Adgate <rosco.adgate@gmail.com>
2025-01-17 08:52:53 -0800

    Playing with operators

--- practice.py
+++ practice.py
@@ -13,3 +13,41 @@ So let's begin.
 
 #1/16/2025  8:36AM PST
 #According to line 3. Step 1 in the process is Mastering Python Basics, which makes sense to me. So what is the first basic that i want to focus on? Or maybe the question is what are the basics? According to chatgpt Syntax and Basic Constructs are #1 so that's where I'll start. Just not right now because I want to smoke a ciggarette and watch TV.
+
+#1/16/2025 9:28AM PST
+some_names = a, b, c, d, e = "Tommy", "Alice", "Bobby", "Chidi", "Ricardo"
+some_variables = a, b, c, d, e, f, g = 1, 2, 3, 4, 5, 6, 7
+best_sushi = {"name": "Shiro's", "location": (47.61477069277289, -122.34742595599339), "origin": "Japan", "price": "High", "quality": "High"}
+# # print(someVariables[3])
+# # b = a/a
+# # print(b)
+
+# for x in some_variables:
+#     # print(x**x)
+
+
+#     def global_keyword():
+#         global c
+#         c = 1
+#         print(c)
+#     global_keyword()
+
+# for x in some_names:
+#     # print(x[0].lower())
+
+# # print(some_names[1], some_variables[1])
+
+#     for x in best_sushi:
+#         sushimi = x
+#     # print(sushimi)
+
+# # print(best_sushi["name"])
+
+#1/17/2025 7:41AM PST
+# user = input('I am python. What is your name? : ')
+# print('Welcome' , user)
+
+# print("Hello,", "Tommy", sep = '... ', end = '!\')
+
+a = (1 + 1) % 3
+print(a)
